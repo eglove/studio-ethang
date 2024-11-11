fe9c39bd35b07ae59e6725b6f48ad5dc25b88a84
Ethan <hello@ethang.email>
2024-11-10 22:02:17 -0600

    Image as reference

--- schemaTypes/block-content.ts
+++ schemaTypes/block-content.ts
@@ -1,15 +1,15 @@
-import {defineType} from 'sanity'
+import { defineType } from "sanity";
 
 export default defineType({
-  name: 'blockContent',
-  title: 'Block Content',
-  type: 'array',
+  name: "blockContent",
+  title: "Block Content",
+  type: "array",
   of: [
     {
       lists: [
         {
-          title: 'Bullet',
-          value: 'bullet',
+          title: "Bullet",
+          value: "bullet",
         },
       ],
       marks: {
@@ -17,59 +17,60 @@ export default defineType({
           {
             fields: [
               {
-                name: 'href',
-                title: 'URL',
-                type: 'url',
+                name: "href",
+                title: "URL",
+                type: "url",
               },
             ],
-            name: 'link',
-            title: 'URL',
-            type: 'object',
+            name: "link",
+            title: "URL",
+            type: "object",
           },
         ],
         decorators: [
           {
-            title: 'Strong',
-            value: 'strong',
+            title: "Strong",
+            value: "strong",
           },
           {
-            title: 'Emphasis',
-            value: 'em',
+            title: "Emphasis",
+            value: "em",
           },
         ],
       },
       styles: [
         {
-          title: 'Normal',
-          value: 'normal',
+          title: "Normal",
+          value: "normal",
         },
         {
-          title: 'H1',
-          value: 'h1',
+          title: "H1",
+          value: "h1",
         },
         {
-          title: 'H2',
-          value: 'h2',
+          title: "H2",
+          value: "h2",
         },
         {
-          title: 'H3',
-          value: 'h3',
+          title: "H3",
+          value: "h3",
         },
         {
-          title: 'H4',
-          value: 'h4',
+          title: "H4",
+          value: "h4",
         },
         {
-          title: 'Quote',
-          value: 'blockquote',
+          title: "Quote",
+          value: "blockquote",
         },
       ],
-      title: 'Block',
-      type: 'block',
+      title: "Block",
+      type: "block",
     },
     {
-      title: 'Image',
-      type: 'imageUpload',
+      title: "Image",
+      type: "reference",
+      to: [{ type: "imageUpload" }],
     },
   ],
-})
+});
